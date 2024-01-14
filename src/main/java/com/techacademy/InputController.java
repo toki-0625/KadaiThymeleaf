4297b58aee267b6d5b39c1e2b52882a3916b9a23
toki-0625 <tokiaya0625@gmail.com>
2024-01-14 23:18:29 +0900

    Lesson15課題再提出

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -3,25 +3,14 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
 
     @GetMapping("/input")
-    public String getInput() {
+    public String getInput(Model model) {
+        model.addAttribute("previous", "val");
         // input.htmlに画面遷移
         return "input";
     }
-    
-    @PostMapping("/previous")
-    public String postForm(@RequestParam("val") String val, Model model) {
-        // フォームから送信されてきた値をModelに登録
-        model.addAttribute("val", val);
-        // confirm.htmlに画面遷移
-        return "previous";
-    }
-    
 }
--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -2,7 +2,6 @@ package com.techacademy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
